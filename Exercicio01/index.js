9c36e0a6683cdb11b2f40b887e84e8425ba1fbbb
RUZZIN <125768983+RUZZIN@users.noreply.github.com>
2023-09-08 14:10:06 -0300

    Envio de atividade

---
+++ Exercicio01/index.js
@@ -0,0 +1,4 @@
+var number1 = 15;
+var number2 = 15;
+var soma = number1 + number2;
+console.log('A soma dos ', number1, ' e ', number2, 'é: ', soma);
---
+++ Exercicio01/index.ts
@@ -0,0 +1,5 @@
+const number1 = 15
+const number2 = 15
+
+const soma = number1 + number2
+console.log('A soma de ', number1, ' e ' , number2, 'é: ', soma)
