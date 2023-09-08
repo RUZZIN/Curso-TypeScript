cf8c9292f794531ec9ef09fb0904e5a66dfc8448
RUZZIN <125768983+RUZZIN@users.noreply.github.com>
2023-09-08 13:56:54 -0300

    Envio e atulização de dados

---
+++ AULA-01/index.js
@@ -0,0 +1,9 @@
+var fistName = "Ruan";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(fistName);
+// greeting(anotherName)
+// greeting(x)
--- AULA-01/index.ts
+++ AULA-01/index.ts
@@ -2,8 +2,10 @@ const fistName = "Ruan"
 const anotherName = 1
 const x = true
 
-function greeting(name) {
-    console.log("Olá" + name)
+function greeting(name: string) {
+    console.log("Olá " + name)
 }
 
 greeting(fistName)
+// greeting(anotherName)
+// greeting(x)
