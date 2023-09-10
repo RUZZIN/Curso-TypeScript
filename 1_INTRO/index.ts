54a841c6dc4dd391613968de88dc1e6c3f2710f7
RUZZIN <125768983+RUZZIN@users.noreply.github.com>
2023-09-10 10:31:39 -0300

    Envio de dados e atualização de dados

--- 1_INTRO/index.ts
+++ 1_INTRO/index.ts
@@ -1,11 +1,11 @@
-const fistName = "Ruan"
+const firstName = 'Matheus'
 const anotherName = 1
 const x = true
 
 function greeting(name: string) {
-    console.log("Olá " + name)
+  console.log('Olá, ' + name)
 }
 
-greeting(fistName)
+greeting(firstName)
 // greeting(anotherName)
 // greeting(x)
