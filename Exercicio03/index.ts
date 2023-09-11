fde8eb39e040e5b92eab2182f031ee33e4163275
RUZZIN <125768983+RUZZIN@users.noreply.github.com>
2023-09-11 14:04:02 -0300

    Envio de exercicios

---
+++ Exercicio03/index.ts
@@ -0,0 +1,27 @@
+function avaliacao(estrelas: number | boolean): string {
+    if (typeof estrelas === 'boolean' && !estrelas) {
+        return 'O usuário não deixou uma avaliação.';
+    }
+
+    if (typeof estrelas === 'number') {
+        switch (estrelas) {
+            case 1:
+                return 'Muito insatisfeito com a experiência.';
+            case 2:
+                return 'Insatisfeito com a experiência.';
+            case 3:
+                return 'Avaliação média.';
+            case 4:
+                return 'Satisfeito com a experiência.';
+            case 5:
+                return 'Muito satisfeito com a experiência.';
+            default:
+                return 'Avaliação inválida!';
+        }
+    }
+
+    return 'Avaliação inválida. Use uma avaliação de 1 a 5 estrelas ou `false`.';
+}
+
+console.log(avaliacao(3));
+console.log(avaliacao(false));
